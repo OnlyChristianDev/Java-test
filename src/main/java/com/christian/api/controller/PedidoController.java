4f5df8a5c729fcece59d315f2c5cb9f3c272bd8e
Christian Gabriel <christiandeveloper123@gmail.com>
2026-03-16 16:44:59 -0300

    Finalizando o crud de pedidos

--- src/main/java/com/christian/api/controller/PedidoController.java
+++ src/main/java/com/christian/api/controller/PedidoController.java
@@ -4,6 +4,8 @@ import com.christian.api.model.Pedido;
 import com.christian.api.service.PedidoService;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/pedido")
 public class PedidoController {
@@ -17,5 +19,20 @@ public class PedidoController {
     @PostMapping
     public Pedido criaPedido(@RequestBody Pedido pedido){
         return service.criaPedido(pedido);
+    }
+
+    @GetMapping("/{id}")
+    public Pedido listaPedidoPorId(@PathVariable Long id){
+        return service.listaPedidoPorId(id);
+    }
+
+    @GetMapping
+    public List<Pedido> listaPedidoPorId(){
+        return service.listaPedido();
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletaPedido(@PathVariable Long id){
+        service.deletaPedido(id);
     }
 }
--- src/main/java/com/christian/api/service/PedidoService.java
+++ src/main/java/com/christian/api/service/PedidoService.java
@@ -4,6 +4,9 @@ import com.christian.api.repository.PedidoRepository;
 import com.christian.api.model.Pedido;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class PedidoService {
     private final PedidoRepository repository;
@@ -14,5 +17,18 @@ public class PedidoService {
 
     public Pedido criaPedido(Pedido pedido) {
         return repository.save(pedido);
+    }
+
+    public Pedido listaPedidoPorId(Long id) {
+        Optional<Pedido> pedido = repository.findById(id);
+        return pedido.orElse(null);
+    }
+
+    public List<Pedido> listaPedido() {
+        return repository.findAll();
+    }
+
+    public void deletaPedido(Long id) {
+        repository.deleteById(id);
     }
 }
